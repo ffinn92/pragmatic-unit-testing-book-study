bf0eb18c6d2e6fadecd3eb1fde47544d9b207512
vans <spentmny@nate.com>
2022-08-16 23:19:57 +0900

    chapter 1.2 : 테스트 fail() 에러 코드 작성

---
+++ src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,14 @@
+package iloveyouboss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
