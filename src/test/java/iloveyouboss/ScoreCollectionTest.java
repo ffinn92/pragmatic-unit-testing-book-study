20ac35542b41d62c8fea27b87fd8b38c69c32444
vans <spentmny@nate.com>
2022-08-16 23:53:44 +0900

    chapter 1.3 : 테스트 코드 ScoreCollectionTest 작성

--- src/test/java/iloveyouboss/ScoreCollectionTest.java
+++ src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -1,13 +1,21 @@
 package iloveyouboss;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class ScoreCollectionTest {
 
     @Test
-    public void test() {
-    }
+    public void answersArithmeticMeanOfTwoNumbers() {
+        // given
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
 
+        // when
+        int actualResult = collection.arithmeticMeae();
+
+        // then
+        assertThat(actualResult).isEqualTo(6);
+    }
 }
