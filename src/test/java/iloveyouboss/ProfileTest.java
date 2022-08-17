7c13b5876a7effc4f547afc916267e14d8a0e0b9
vans <spentmny@nate.com>
2022-08-17 13:30:33 +0900

    chapter 2.4 : 유지 보수성을 고려한 예제 코드 작성
    - 각 단위 테스트는 고유 맥락을 갖으므로 테스트 2개를 위해 Profile, Question 객체를 따로 생성

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -22,4 +22,21 @@ class ProfileTest {
 
         assertThat(matches).isFalse();
     }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got milk?");
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertThat(matches).isTrue();
+    }
 }
