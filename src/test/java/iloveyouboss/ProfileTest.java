8b1b0f37e89398978415b1ccb2c69bbcdf6081a6
vans <spentmny@nate.com>
2022-08-20 00:45:25 +0900

    chapter 4.4 : 분리되어 있는 테스트들 주석으로만 분리해보기
    - 테스트 분리시 공통적으로 실행되는 반복적인 초기화의 부담이 줄어든다.
    - 하지만, 분리했을때의 목적성이 뚜렸하고, 프로그래머에게 사용 편의성을 증가시켜준다.

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -17,6 +17,27 @@ class ProfileTest {
         criteria = new Criteria();
     }
 
+    @Test
+    public void matches() {
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got milk?");
+
+        // must-match 항목이 맞지 않으면 false
+        profile.add(new Answer(question, Bool.FALSE));
+        Criteria criteria = new Criteria();
+        criteria.add(
+                new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
+
+        assertThat(profile.matches(criteria)).isFalse();
+
+        // don't care 항목에 대해서는 true
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria = new Criteria();
+        criteria.add(
+                new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+
+        assertThat(profile.matches(criteria)).isTrue();
+    }
 
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
